77c1e8100834284ad37ce13344772066641c0138
Devin Rasmussen <devinr@anglepoint.com>
2018-05-15 16:36:29 -0600

    Insert MuiThemeProvider on root app as required by Material UI

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  </BrowserRouter>, 
+  document.getElementById('root'));
 registerServiceWorker();
