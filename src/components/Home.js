fbd245b643fd389fdbe1913110595a0c52995402
Devin Rasmussen <devinr@anglepoint.com>
2018-05-17 08:42:38 -0600

    move table to its own component, add react-loading

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,46 @@ import {
   TableRowColumn,
 } from 'material-ui/Table';
 import Moment from 'react-moment';
+import {Scoped} from 'kremling';
+import ReactLoading from 'react-loading';
 
+const css = `
+  & a {
+    text-decoration: none;
+    color: #0235d0;
+  }
+`
+const ReposTable = (props) => {
+  return (
+    <Table
+        selectable={false}
+        showCheckboxes={false}>
+        <TableHeader 
+          displaySelectAll={false}
+          adjustForCheckbox={false}
+        >
+          <TableRow>
+            <TableHeaderColumn>Stars</TableHeaderColumn>
+            <TableHeaderColumn>Name</TableHeaderColumn>
+            <TableHeaderColumn>Open Issues</TableHeaderColumn>
+            <TableHeaderColumn>Last Updated</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false}>
+          {props.repos.map((repo) => (
+            <TableRow key={repo.id}>
+              <Scoped css={css}>
+                <TableRowColumn>{repo.stargazers_count}</TableRowColumn>
+                <TableRowColumn><a href={`${repo.html_url}`}>{repo.name}</a></TableRowColumn>
+                <TableRowColumn><a href={`${repo.html_url}/issues`}>{repo.open_issues_count}</a></TableRowColumn>
+                <TableRowColumn><Moment format="DD MMM YYYY">{repo.pushed_at}</Moment></TableRowColumn>
+              </Scoped>
+            </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
 class Home extends Component {
   constructor(props) {
     super(props)
@@ -25,36 +64,20 @@ class Home extends Component {
     }) 
   }
   render() {
+    const loaderStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center', /* center items vertically, in this case */
+      alignItems: 'center',    /* center items horizontally, in this case */
+      height: '300px'
+    }
     return (
       <div>
         <h2>Canopy JS Repos by stars</h2>
-        <Table
-          selectable={false}
-          showCheckboxes={false}>
-          <TableHeader 
-            displaySelectAll={false}
-            adjustForCheckbox={false}
-          >
-            <TableRow>
-              <TableHeaderColumn>Stars</TableHeaderColumn>
-              <TableHeaderColumn>Name</TableHeaderColumn>
-              <TableHeaderColumn>Open Issues</TableHeaderColumn>
-              <TableHeaderColumn>Last Updated</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody displayRowCheckbox={false}>
-            {this.state.repos ? 
-              this.state.repos.map((repo) => (
-                <TableRow key={repo.id}>
-                  <TableRowColumn>{repo.stargazers_count}</TableRowColumn>
-                  <TableRowColumn><a href={`${repo.html_url}`}>{repo.name}</a></TableRowColumn>
-                  <TableRowColumn><a href={`${repo.html_url}/issues`}>{repo.open_issues_count}</a></TableRowColumn>
-                  <TableRowColumn><Moment format="DD MMM YYYY">{repo.pushed_at}</Moment></TableRowColumn>
-                </TableRow>
-            ))
-            : null}
-          </TableBody>
-        </Table>
+        {this.state.repos 
+          ? <ReposTable repos={this.state.repos} />
+          : <div style={loaderStyle}><ReactLoading type="spin" color="0235d0" /></div>
+        }
       </div>
     );
   }
